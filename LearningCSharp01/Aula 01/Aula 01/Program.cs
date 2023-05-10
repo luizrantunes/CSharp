24336a926acb029a28ded91091c27887f89cfaa9
Luiz Antunes <luizrantunes@outlook.com>
2023-05-10 11:31:34 -0300

    Adicionar arquivos de projeto.

---
+++ LearningCSharp01/Aula 01/Aula 01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Opaaa!");
+        }
+    }
+}
