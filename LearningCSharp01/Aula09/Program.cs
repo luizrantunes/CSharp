afb1a4b077491e869c7a07e21caf1ec9f5ee67fe
Luiz Antunes <luizrantunes@outlook.com>
2023-05-18 00:02:54 -0300

    add aula09

---
+++ LearningCSharp01/Aula09/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula09
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            double[] vetor = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(vetor[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
