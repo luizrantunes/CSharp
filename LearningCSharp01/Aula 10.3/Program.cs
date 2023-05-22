87ee19b96ebe1ea896921ff595be9ca95aaba1bd
Luiz Antunes <luizrantunes@outlook.com>
2023-05-22 15:02:26 -0300

    update aula10

---
+++ LearningCSharp01/Aula 10.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula10
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int M = int.Parse(s[0]);
+            int N = int.Parse(s[1]);
+            int[,] mat = new int[M, N];
+
+            // esse primeiro for percorre as linhas
+            for (int i = 0; i < M; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+
+                // ja esse segundo for percorre as colunas
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            int[] vet = new int[M];
+
+            for (int i = 0; i < M; i++)
+            {
+                int soma = 0;
+                for (int j = 0; j < N; j++)
+                {
+                    soma += mat[i, j];
+                }
+                vet[i] = soma;
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+        }
+    }
+}
---
+++ LearningCSharp01/Aula10.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula10
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[,] A = new int[N, N];
+
+            // esse primeiro for percorre as linhas
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+
+                // ja esse segundo for percorre as colunas
+                for (int j = 0; j < N; j++)
+                {
+                    A[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL:");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(A[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int cont = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (A[i, j] < 0)
+                    {
+                        cont++;
+                    }
+                }
+            }
+            Console.WriteLine("QUANTIDADE DE NEGATIVOS = " + cont);
+        }
+    }
+}
