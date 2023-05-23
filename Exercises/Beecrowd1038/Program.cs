e33a6cca0683b5a4255050c32920af4ef5e3c3e9
Luiz Antunes <luizrantunes@outlook.com>
2023-05-23 16:03:06 -0300

    git add beecrowd1038 e 1040

---
+++ Exercises/Beecrowd1038/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1038
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dados = Console.ReadLine().Split(' ');
+            int lanche = int.Parse(dados[0]);
+            int quantidade = int.Parse(dados[1]);
+            double valor = 0.00;
+            double valorTotal = 0.00;
+
+            if (lanche == 1)
+            {
+                valor = 4.00;
+                valorTotal = valor * quantidade;
+            }
+            else if (lanche == 2)
+            {
+                valor = 4.50;
+                valorTotal = valor * quantidade;
+            }
+            else if (lanche == 3)
+            {
+                valor = 5.00;
+                valorTotal = valor * quantidade;
+            }
+            else if (lanche == 4)
+            {
+                valor = 2.00;
+                valorTotal = valor * quantidade;
+            }
+            else if (lanche == 5)
+            {
+                valor = 1.50;
+                valorTotal = valor * quantidade;
+            }
+            else
+                Console.WriteLine("Produto e quantidade invalidos");
+
+            Console.WriteLine("Total: R$ " + valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercises/Beecrowd1040/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1040
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] notas = Console.ReadLine().Split(' ');
+            double N1 = double.Parse(notas[0], CultureInfo.InvariantCulture);
+            double N2 = double.Parse(notas[1], CultureInfo.InvariantCulture);
+            double N3 = double.Parse(notas[2], CultureInfo.InvariantCulture);
+            double N4 = double.Parse(notas[3], CultureInfo.InvariantCulture);
+
+            int peso1 = 2;
+            int peso2 = 3;
+            int peso3 = 4;
+            int peso4 = 1;
+
+            double media = (N1 * peso1 + N2 * peso2 + N3 * peso3 + N4 * peso4) / (peso1 + peso2 + peso3 + peso4);
+
+            if (media >= 7.0)
+            {
+                Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno aprovado.");
+            }
+            else if (media >= 5.00 && media < 7.0)
+            {
+                Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno em exame.");
+                double notaExame = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                double mediaExame = (notaExame + media) / 2;
+                Console.WriteLine("Nota do exame: " + notaExame.ToString("F1", CultureInfo.InvariantCulture));
+                if (mediaExame >= 5.0)
+                {
+                    Console.WriteLine("Aluno aprovado.");
+                    Console.WriteLine("Media final: " + mediaExame.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Media: " + (media-0.01).ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno reprovado.");
+            }
+        }
+    }
+}
