54045418fc78d49cb17b7be38d85505de96fe0d4
Luiz Antunes <luizrantunes@outlook.com>
2023-05-24 15:41:30 -0300

    add beecrowd1043 1044 e 1046

---
+++ Exercises/Beecrowd1043/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1043
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            double A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            if (A + B > C && A + C > B && B + C > A)
+            {
+                double perimetro = A + B + C;
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                double area = ((A + B) * C) / 2;
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ Exercises/Beecrowd1046/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beecrowd1046
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int inicial = int.Parse(valores[0]);
+            int final = int.Parse(valores[1]);
+            int duracao = 0;
+
+            if (inicial < final)
+            {
+                duracao = final - inicial;
+            }
+            else
+            {
+                duracao = 24 - inicial + final;
+            }
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
