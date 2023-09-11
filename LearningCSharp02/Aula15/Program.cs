20fd920e0d8b59d73701af531ad010c97ae36403
Luiz Antunes <luizrantunes@outlook.com>
2023-09-11 18:02:44 -0300

    update Aula15

---
+++ LearningCSharp02/Aula15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
